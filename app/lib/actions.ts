888fa861990f12cc857eb75aa38c00892866e9c8
Italo Nabuco <italonabuco@hotmail.com>
2024-02-05 12:12:01 -0300

    12: mutating data (editing invoice)

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -18,6 +18,8 @@ const FormSchema = z.object({
 
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+
 export async function createInvoice(formData: FormData) {
   const { amount, customerId, status } = CreateInvoice.parse({
     customerId: formData.get('customerId'),
@@ -34,6 +36,25 @@ export async function createInvoice(formData: FormData) {
   `;
 
   //   you want to clear this cache and trigger a new request to the server
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
+}
+
+export async function updateInvoice(id: string, formData: FormData) {
+  const { amount, customerId, status } = UpdateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+
+  const amountInCents = amount * 100;
+
+  await sql`
+        UPDATE invoices 
+        SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+        WHERE id = ${id}
+      `;
+
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 }
